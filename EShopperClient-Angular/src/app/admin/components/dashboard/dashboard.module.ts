d458b51db82aab51af8e3ecf6893a08ab64be233
Egemen Tosyalı <egementosyalitr@gmail.com>
2022-12-29 16:31:01 +0300

    Add routings

--- EShopperClient-Angular/src/app/admin/components/dashboard/dashboard.module.ts
+++ EShopperClient-Angular/src/app/admin/components/dashboard/dashboard.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BasketsComponent } from './baskets.component';
+import { DashboardComponent } from './dashboard.component';
 import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    BasketsComponent
+    DashboardComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path:"",component: BasketsComponent}
+      {path:"",component: DashboardComponent}
     ])
   ]
 })
-export class BasketsModule { }
+export class DashboardModule { }
