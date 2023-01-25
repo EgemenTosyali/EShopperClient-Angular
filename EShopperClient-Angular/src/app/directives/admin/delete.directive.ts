5a164fa1dc182c6b660f742f77b4c7c0cb5e2e33
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-01-26 00:54:24 +0300

    Add spinner & Toastr to delete directive

--- EShopperClient-Angular/src/app/directives/admin/delete.directive.ts
+++ EShopperClient-Angular/src/app/directives/admin/delete.directive.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { CustomToastrService, MessagePosition, MessageType } from 'src/app/services/alerts/custom-toastr.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 declare var $: any
@@ -10,7 +11,10 @@ declare var $: any
 export class DeleteDirective {
 
   constructor(
-    private element: ElementRef, private _renderer: Renderer2, private httpClientService: HttpClientService, private customToastr: CustomToastrService) {
+    private element: ElementRef, private _renderer: Renderer2,
+    private httpClientService: HttpClientService,
+    private customToastr: CustomToastrService,
+    private spinner: NgxSpinnerService) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete_icon.png");
     img.setAttribute("style", "cursor: pointer;")
@@ -23,14 +27,20 @@ export class DeleteDirective {
   @Input() controller: string;
   @HostListener("click")
   async onClick() {
+    this.spinner.show("fire");
     const td: HTMLTableCellElement = this.element.nativeElement;
     this.httpClientService.delete({
       controller: this.controller
     }, this.id).subscribe(data => {
-      alert("done")
+      this.spinner.hide("fire");
       this.callback.emit();
+      this.customToastr.message("Product Deleted Succesfully", "Success", {
+        messagePosition: MessagePosition.TopRight,
+        messageType: MessageType.Success
+      })
     }, (errorResponse: HttpErrorResponse) => {
-      this.customToastr.message(`${errorResponse}`,"error",{
+      this.spinner.hide("fire");
+      this.customToastr.message(`${errorResponse}`, "error", {
         messagePosition: MessagePosition.TopCenter,
         messageType: MessageType.Error
       })
