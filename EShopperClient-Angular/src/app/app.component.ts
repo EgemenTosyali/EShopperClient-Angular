416487823a094e3cd547b4fe361909a9f4bd27ba
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-01-27 01:08:28 +0300

    Update slider only visible on home page

--- EShopperClient-Angular/src/app/app.component.ts
+++ EShopperClient-Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EShopperClient-Angular';
+  visibleSlider: boolean = false;
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      this.visibleSlider = router.url == "/" ? true : false;
+    })
+  }
+
 }
