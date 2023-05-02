32ddd773ef67b6fb460c1960424451f11e943b71
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-05-02 04:18:32 +0300

    feat: add login feature to user services

--- EShopperClient-Angular/src/app/services/common/user.service.ts
+++ EShopperClient-Angular/src/app/services/common/user.service.ts
@@ -3,19 +3,46 @@ import { HttpClientService } from './http-client.service';
 import { User } from 'src/entities/user';
 import { Create_User } from 'src/app/contracts/product/users/create_user';
 import { Observable, firstValueFrom } from 'rxjs';
+import { TokenResponse } from 'src/app/contracts/tokens/token-response';
+import { CustomToastrService, MessagePosition, MessageType } from '../alerts/custom-toastr.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(
+    private httpClientService: HttpClientService,
+    private customToastr: CustomToastrService) { }
 
-  async create(user: User){
+  async create(user: User) {
     const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
       controller: "users"
-    },user)
+    }, user)
 
     return await firstValueFrom(observable) as Create_User
+  }
+
+  async login(usernameOrEmail: string, password: string, callBackFunc?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
+      controller: "users",
+      action: "login"
+    }, { usernameOrEmail, password })
+
+    const tokenRespose: TokenResponse = await firstValueFrom(observable)
+    if (tokenRespose) {
+      localStorage.setItem("accessToken", tokenRespose.token.accessToken)
+      this.customToastr.message("Welcome!", "Welcome!", {
+        messagePosition: MessagePosition.TopRight,
+        messageType: MessageType.Success
+      })
+    }
+    else{
+      this.customToastr.message("Login Failed!", "Login Failed!", {
+        messagePosition: MessagePosition.TopRight,
+        messageType: MessageType.Error
+      })
+    }
+    callBackFunc()
   }
 }
