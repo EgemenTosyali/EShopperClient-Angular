12e6ce31b0e87f6182da44f2b980f951731d0fbb
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-05-30 23:22:01 +0300

    feat: add log out button
    delete access token from local storage while logging out

--- EShopperClient-Angular/src/app/admin/layout/components/header/header.component.ts
+++ EShopperClient-Angular/src/app/admin/layout/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomToastrService, MessagePosition, MessageType } from 'src/app/services/alerts/custom-toastr.service';
 import { AuthService } from 'src/app/services/common/auth.service';
 
 @Component({
@@ -8,6 +10,15 @@ import { AuthService } from 'src/app/services/common/auth.service';
 })
 export class HeaderComponent {
   authCheck: boolean
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private router: Router, private customToastr: CustomToastrService) {
+  }
+  signOut() {
+    localStorage.removeItem("accessToken")
+    this.authService.authChecker()
+    this.router.navigate([""])
+    this.customToastr.message("Successfuly logged out", "Log out", {
+      messagePosition: MessagePosition.TopRight,
+      messageType: MessageType.Success
+    })
   }
 }
